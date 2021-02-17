035e09309c4535d0ceec24617c17845e2279abab
adospinoiu <alex.dospinoiu@gmail.com>
2021-02-16 19:45:44 -0700

    Instructions complete for addEventListener

--- script.js
+++ script.js
@@ -7,8 +7,21 @@ const child = document.querySelector(".child")
 // The addEventListener takes the event (i.e. CLICK) and a call back function that is to be performed every time that event occurs
 grandparent.addEventListener("click", e => {
     //This will console.log information from the DOM
-    console.log(e);
+    // console.log(e);
     //This will console.log the <div> that is clicked
-    console.log(e.target);
+    // console.log(e.target);
 
+    console.log("Grandparent")
+});
+
+parent.addEventListener("click", e => {
+    console.log("Parent")
+});
+
+child.addEventListener("click", e => {
+    console.log("Child")
+})
+
+document.addEventListener("click", e => {
+    console.log("Document")
 })
