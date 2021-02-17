c75d38174c8abfb825f85d68dcf810a30d6efcee
adospinoiu <alex.dospinoiu@gmail.com>
2021-02-16 21:12:48 -0700

    Added code to script.js that captures up and bubbles out on all three divs

--- script.js
+++ script.js
@@ -5,23 +5,61 @@ const child = document.querySelector(".child")
 // Add a Click_Event_Listener
 // There are other event listeners available besides a CLICK
 // The addEventListener takes the event (i.e. CLICK) and a call back function that is to be performed every time that event occurs
-grandparent.addEventListener("click", e => {
-    //This will console.log information from the DOM
-    // console.log(e);
-    //This will console.log the <div> that is clicked
-    // console.log(e.target);
 
-    console.log("Grandparent")
-});
+// grandparent.addEventListener("click", e => {
+//     //This will console.log information from the DOM
+//     // console.log(e);
+//     //This will console.log the <div> that is clicked
+//     // console.log(e.target);
+
+//     console.log("Grandparent")
+// });
+
+// parent.addEventListener("click", e => {
+//     console.log("Parent")
+// });
+
+// child.addEventListener("click", e => {
+//     console.log("Child")
+// })
+
+// document.addEventListener("click", e => {
+//     console.log("Document")
+// })
+
+// ##############################################################
+
+// Add a event Bubbling/Capturing
+// Bubble working "from in to out" and Capture works "from out to in"
+grandparent.addEventListener("click", e => {
+    console.log("Grandparent Capture")
+}, { capture: true })
+
+grandparent.addEventListener("click", e => {
+    console.log("Grandparent Bubble")
+})
 
 parent.addEventListener("click", e => {
-    console.log("Parent")
-});
+    console.log("Parent Capture")
+}, { capture: true })
+
+parent.addEventListener("click", e => {
+    console.log("Parent Bubble")
+})
 
 child.addEventListener("click", e => {
-    console.log("Child")
+    console.log("Child Capture")
+}, { capture: true })
+
+child.addEventListener("click", e => {
+    console.log("Child Bubble")
 })
 
 document.addEventListener("click", e => {
-    console.log("Document")
+    console.log("Document Capture")
+}, { capture: true })
+
+document.addEventListener("click", e => {
+    console.log("Document Bubble")
 })
+
