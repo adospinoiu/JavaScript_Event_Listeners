0653f74ef95ea3d8d2f09486e98fa2c2bd60ab3c
adospinoiu <alex.dospinoiu@gmail.com>
2021-02-16 21:21:41 -0700

    Added code to script.js that shows how to run an even listener only once

--- script.js
+++ script.js
@@ -31,35 +31,55 @@ const child = document.querySelector(".child")
 
 // Add a event Bubbling/Capturing
 // Bubble working "from in to out" and Capture works "from out to in"
-grandparent.addEventListener("click", e => {
-    console.log("Grandparent Capture")
-}, { capture: true })
+// grandparent.addEventListener("click", e => {
+//     console.log("Grandparent Capture")
+// }, { capture: true })
 
+// grandparent.addEventListener("click", e => {
+//     console.log("Grandparent Bubble")
+// })
+
+// parent.addEventListener("click", e => {
+//     console.log("Parent Capture")
+// }, { capture: true })
+
+// parent.addEventListener("click", e => {
+//     console.log("Parent Bubble")
+// })
+
+// child.addEventListener("click", e => {
+//     console.log("Child Capture")
+//     //To STOP the propagation on this step. (Movable to any other step)
+//     e.stopPropagation()
+// }, { capture: true })
+
+// child.addEventListener("click", e => {
+//     console.log("Child Bubble")
+// })
+
+// document.addEventListener("click", e => {
+//     console.log("Document Capture")
+// }, { capture: true })
+
+// document.addEventListener("click", e => {
+//     console.log("Document Bubble")
+// })
+
+// ############################################################
+
+// To run an event only once add the { once: true }
 grandparent.addEventListener("click", e => {
     console.log("Grandparent Bubble")
 })
 
 parent.addEventListener("click", e => {
-    console.log("Parent Capture")
-}, { capture: true })
-
-parent.addEventListener("click", e => {
     console.log("Parent Bubble")
-})
-
-child.addEventListener("click", e => {
-    console.log("Child Capture")
-}, { capture: true })
+}, { once: true })
 
 child.addEventListener("click", e => {
     console.log("Child Bubble")
 })
 
-document.addEventListener("click", e => {
-    console.log("Document Capture")
-}, { capture: true })
+// #############################################################
 
-document.addEventListener("click", e => {
-    console.log("Document Bubble")
-})
-
+// To run an event as many times as you want .. and then remove it
